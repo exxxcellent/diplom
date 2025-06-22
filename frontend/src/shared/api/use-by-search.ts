4b37f0f84e24b335322f9ceffa51e3d8fa3f0343
exxxcellent <aleksejdikun@mail.ru>
2025-06-22 13:52:02 +0300

    refactor: remove unused DateType import and related code from useBySearch

--- frontend/src/shared/api/use-by-search.ts
+++ frontend/src/shared/api/use-by-search.ts
@@ -1,4 +1,4 @@
-import { DateType, SearchType, SortType } from '@/widgets/search-news';
+import { SearchType, SortType } from '@/widgets/search-news';
 import { useQuery } from '@tanstack/react-query';
 import { chooseSortType } from '../helpers/choose-search-type';
 import { chooseSearchType } from '../helpers/choose-sort-type copy';
@@ -9,13 +9,10 @@ const fetchBySearch = async (
     q: string,
     sort: SortType,
     search: SearchType,
-    // dates: DateType | null,
     page?: number
 ) => {
     const sortParam = chooseSortType(sort);
     const searchParam = chooseSearchType(search);
-    // const from = dates ? dates[0] : null;
-    // const to = dates ? dates[1] : null;
 
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/news/everything?q=${q}&page=${page}&sortBy=${sortParam}&searchIn=${searchParam}`
@@ -32,7 +29,6 @@ export const useBySearch = (
     q: string,
     sort: SortType,
     search: SearchType,
-    dates: DateType | null,
     page?: number
 ) => {
     const {
